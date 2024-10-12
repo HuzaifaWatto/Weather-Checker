fac5fc213924b1e333c57c9b6f94a6be5296260d
HuzaifaWatto <98208958+HuzaifaWatto@users.noreply.github.com>
2024-10-12 14:14:02 +0500

    Update Weather.py
    removal of personel API

--- Weather App/Scripts/Weather.py
+++ Weather App/Scripts/Weather.py
@@ -5,7 +5,7 @@ import win32com.client
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 x = input("Enter the city name for weather update's: " )
 
-url = f"http://api.weatherapi.com/v1/current.json?key=dbbfae24ddef452fb3773647242908&q={x}"
+url = f"http://api.weatherapi.com/v1/current.json?key=key={x}"
 r = requests.get(url)
 dic = json.loads(r.text)
 w = dic["current"]["temp_c"]
